4b88d261faa7ff0f3026ca6180d0445719fbbb51
svcoverman <vsaul0711@gmail.com>
2020-01-13 22:57:48 -0600

    trying to run live github page

---
+++ build/precache-manifest.8ad71789f382bd85512b224878968c89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f4a6e96c39a80f7f8423997906f027",
+    "url": "/clicky-xeno/index.html"
+  },
+  {
+    "revision": "965d66f7e941ba747033",
+    "url": "/clicky-xeno/static/css/main.369f3c07.chunk.css"
+  },
+  {
+    "revision": "24f554ff02ba722404f6",
+    "url": "/clicky-xeno/static/js/2.5f57dac6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-xeno/static/js/2.5f57dac6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "965d66f7e941ba747033",
+    "url": "/clicky-xeno/static/js/main.588123b7.chunk.js"
+  },
+  {
+    "revision": "e144af0612c921717dbb",
+    "url": "/clicky-xeno/static/js/runtime-main.4ee8151f.js"
+  }
+]);
